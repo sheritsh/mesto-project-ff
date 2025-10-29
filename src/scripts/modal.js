16baca3a3d3092558d778eeb7b0f74dc428da302
Oleg Polovinko <sheritsh.s21@gmail.com>
2025-10-29 12:31:30 +0300

    feat: add close modal events

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,5 +1,24 @@
 const openModal = (modalElem) => {
   modalElem.classList.add('popup_is-opened');
+
+  const onCloseClick = (evt) => {
+    if (evt.target.matches('.popup__close') || evt.target.matches('.popup_is-opened'))
+      closeModal(modalElem, onCloseClick, onEscKeydown);
+  };
+
+  const onEscKeydown = (evt) => {
+    if (evt.key === 'Escape') closeModal(modalElem, onCloseClick, onEscKeydown);
+  };
+
+  modalElem.addEventListener('click', onCloseClick);
+  document.addEventListener('keydown', onEscKeydown);
+};
+
+const closeModal = (modalElem, onCloseClick, onEscKeydown) => {
+  modalElem.classList.remove('popup_is-opened');
+
+  modalElem.removeEventListener('click', onCloseClick);
+  document.removeEventListener('keydown', onEscKeydown);
 };
 
 export { openModal };
